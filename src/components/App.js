bdf38c3275e2dbcbc63f9afa3b92779a80e6240b
reddy-sravan12 <sravankumarreddy102@gmail.com>
2025-06-06 11:51:45 +0530

    debug

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 
+import React from 'react';
 
 const App = () => {
   return (
-    <div id='main'>
+    <div id="main">
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
     </div>
